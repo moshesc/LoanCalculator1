e7e6fd1b30445df12a9c8fff59538dfbbbd3c7ad
moshesc <beepbeep9860@gmail.com>
2017-08-26 16:36:22 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanCalculator.Models;
+using Chart.Mvc.SimpleChart;
+
+namespace LoanCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new Loan(0, 0, 0));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public PartialViewResult CalculateResults(Loan loan)
+        {
+            LoanViewModel Model = new LoanViewModel(loan);
+            bool ShowResults = false;
+            if (ModelState.IsValid)
+            {
+                decimal MonthlyInterestRate = (loan.APR / 12) / 100;
+                decimal MonthlyRepayment = CalculateMonthlyRepayment(loan.Amount, MonthlyInterestRate, loan.TermInMonths);
+                Model.MonthlyRepayment = MonthlyRepayment;
+                decimal Repayment = MonthlyRepayment;
+
+                decimal OutstandingBalance = loan.Amount;
+                decimal Interest = 0;
+                decimal TotalInterest = 0;
+
+                Model.OpeningBalances = new decimal[loan.TermInMonths];
+                Model.Debits = new decimal[loan.TermInMonths];
+                Model.Credits = new decimal[loan.TermInMonths];
+                Model.ClosingBalances = new decimal[loan.TermInMonths];
+
+                for (int i = 0; i < loan.TermInMonths; i++)
+                {
+                    Model.OpeningBalances[i] = OutstandingBalance;
+
+                    Interest = Math.Round(OutstandingBalance * MonthlyInterestRate, 2);
+                    OutstandingBalance += Interest;
+                    TotalInterest += Interest;
+                    Model.Debits[i] = Interest;
+
+                    if (i == (loan.TermInMonths - 1))
+                    {
+                        Repayment = OutstandingBalance;
+                    }
+                    OutstandingBalance -= Repayment;
+                    Model.Credits[i] = Repayment;
+
+                    Model.ClosingBalances[i] = OutstandingBalance;
+                }
+
+                ShowResults = true;
+                Model.TotalInterest = TotalInterest;
+                Model.TotalRepaid = loan.Amount + TotalInterest;
+
+                PieChart chart = new PieChart();
+                chart.Data.AddRange(new List<SimpleData>
+                {
+                    new SimpleData
+                    {
+                        Value = (double)Math.Round(TotalInterest, 2),
+                        Label = "Interest",
+                        Color = "rgba(220, 0, 0, 0.5)",
+                    },
+                    new SimpleData()
+                    {
+                        Value = (double)Math.Round(loan.Amount, 2),
+                        Label = "Capital",
+                        Color = "rgba(0, 220, 0, 0.5)"
+                    }});
+                Model.PieChart = chart;
+            }
+            ViewBag.ShowResults = ShowResults;
+
+            return PartialView("Results", Model);
+        }
+
+        private decimal CalculateMonthlyRepayment(decimal Amount, decimal MonthlyInterestRate, int TermInMonths)
+        {
+            decimal payment = 0;
+            decimal power = (decimal)Math.Pow((double)(1 + MonthlyInterestRate), (TermInMonths * -1));
+            decimal denominator = 1 - power;
+            decimal numerator = Amount * MonthlyInterestRate;
+            payment = numerator / denominator;
+
+            return Math.Round(payment, 2);
+        }
+    }
+}
---
+++ Models/Loan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoanCalculator.Models
+{
+    public class Loan
+    {
+        [Required(ErrorMessage = "You must enter and amount")]
+        [Range(500, Double.MaxValue, ErrorMessage = "The loan must be at least £500")]
+        public decimal Amount { get; set; }
+        [Required(ErrorMessage = "You must enter an interest rate")]
+        [Range(0.5, 100, ErrorMessage = "The interest rate must be between 0.5% and 100%")]
+        [Display(Name = "Interest rate")]
+        public decimal APR { get; set; }
+        [Display(Name = "Term in months")]
+        [Required(ErrorMessage = "You must enter a term")]
+        public int TermInMonths { get; set; }
+        public Loan() { }
+        public Loan(decimal amount, decimal apr, int term)
+        {
+            Amount = amount;
+            APR = apr;
+            TermInMonths = term;
+        }
+
+    }
+}
